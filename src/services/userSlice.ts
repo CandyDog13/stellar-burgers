9d645dd37b472bfa78c4e5261b8efb95c4082114
CandyDog13 <kga13@yandex.ru>
2024-08-05 19:59:52 +0300

    jest tests: rootReducer - done, burgerConstructor - done

--- src/services/userSlice.ts
+++ src/services/userSlice.ts
@@ -9,7 +9,7 @@ import {
   TLoginData,
   TRegisterData,
   updateUserApi
-} from '@api';
+} from '../utils/burger-api';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { TUser, TOrder } from '@utils-types';
 import { deleteCookie, getCookie, setCookie } from '../utils/cookie';
